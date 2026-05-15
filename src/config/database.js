79548248cf2ce8bca7e9f8572e04c00a3cd7ab4f
brunodomings <brunodomingossantos@gmail.com>
2026-05-14 21:14:13 -0300

    post,put, delete

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 const MONGO_URI =
-  'mongodb+srv://brunodomingossantos_db_user:XI2hB3LturT7W8oM@cluster0.avahtsv.mongodb.net/Estudo?appName=Cluster0';
+  'mongodb://brunodomingossantos_db_user:XI2hB3LturT7W8oM@ac-cq8g64x-shard-00-00.avahtsv.mongodb.net:27017,ac-cq8g64x-shard-00-01.avahtsv.mongodb.net:27017,ac-cq8g64x-shard-00-02.avahtsv.mongodb.net:27017/Estudo?ssl=true&replicaSet=atlas-qc1vyn-shard-0&authSource=admin&appName=Cluster0';
 
 async function connectDatabase() {
   try {
     await mongoose.connect(MONGO_URI);
-    console.log(' Banco conectado');
+    console.log('Banco conectado');
   } catch (error) {
     console.error('Erro ao conectar no banco:', error.message);
   }
